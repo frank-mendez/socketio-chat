68f5a5bcb44a30b52e50608dd98fbf45b2bfd7ce
frankmendez <frankmendezwebdev@gmail.com>
2021-01-27 23:55:53 +0800

    Install Modules For Project Setup

--- server.js
+++ server.js
@@ -3,8 +3,20 @@ const bodyParser = require('body-parser')
 const ejs = require('ejs')
 const http = require('http')
 const container = require('./container')
+const cookieParser = require('cookie-parser')
+const validator = require('express-validator')
+const session = require('express-session')
+const MongoStore = require('connect-mongo')(session)
+const mongoose = require('mongoose')
+const flash = require('connect-flash')
 
 container.resolve(function (users) {
+	mongoose.Promise = global.Promise
+	mongoose.connect(
+		'mongodb+srv://socketiochat:socketiochat@cluster0.j8lzv.mongodb.net/socketiochat?retryWrites=true&w=majority',
+		{ useNewUrlParser: true }
+	)
+
 	const app = SetupExpress()
 
 	function SetupExpress() {
@@ -25,8 +37,20 @@ container.resolve(function (users) {
 
 	function ConfigureExpress(app) {
 		app.use(express.static('public'))
+		app.use(cookieParser())
 		app.set('view engine', 'ejs')
 		app.use(bodyParser.json())
 		app.use(bodyParser.urlencoded({ extended: true }))
+
+		app.use(validator())
+		app.use(
+			session({
+				secret: 'secret-key',
+				resave: true,
+				saveUninitialized: true,
+				store: new MongoStore({ mongooseConnection: mongoose.connection }),
+			})
+		)
+		app.use(flash())
 	}
 })
