113f054cab8ca5ee041adf4862fe45c7085afe15
Frank Mendez <frankmendezresources@gmail.com>
2024-04-15 20:55:00 +0800

    chore: test client base url

--- client/src/api/lib/createBaseAxiosInstance.ts
+++ client/src/api/lib/createBaseAxiosInstance.ts
@@ -4,7 +4,7 @@ export const createBaseAxiosInstance = (
     configOverrides: CreateAxiosDefaults = {}
 ) => {
     return axios.create({
-        baseURL: process.env.VITE_HOST_API,
+        baseURL: 'http://localhost:3000',
         ...configOverrides,
     });
 };
